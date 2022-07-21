50971fc913ae4f27c0f2051f0359b2170bff1c04
gsv <rsdpisuy@gmail.com>
2022-07-21 08:54:11 +0300

    Code cleanup.

--- VSharp.SILI/SILI.fs
+++ VSharp.SILI/SILI.fs
@@ -192,7 +192,7 @@ type public SILI(options : SiliOptions) =
             | Stop -> __unreachable__()
 
     member private x.AnswerPobs entryPoint cmdArgs initialStates =
-        CFG.applicationGraph.Reset()
+        CFG.applicationGraph.ResetQueryEngine()
         CFG.applicationGraph.RegisterMethod entryPoint
         statistics.ExplorationStarted()
         branchesReleased <- false
